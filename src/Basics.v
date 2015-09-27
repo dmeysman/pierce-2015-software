0ab9eb0618734ba0d79af09a7e63522bcd2356ea
Dylan Meysmans <dmeysman@vub.ac.be>
2015-09-27 18:15:49 +0200

    Prove negation_fn_applied_twice

--- src/Basics.v
+++ src/Basics.v
@@ -872,7 +872,18 @@ Qed.
     to the previous one but where the second hypothesis says that the
     function [f] has the property that [f x = negb x].*)
 
-(* FILL IN HERE *)
+Theorem negation_fn_applied_twice :
+  forall (f : bool -> bool),
+  (forall (x : bool), f x = negb x) ->
+  forall (b : bool), f (f b) = b.
+Proof.
+  intros f H b.
+  rewrite -> H.
+  rewrite -> H.
+  destruct b.
+  reflexivity.
+  reflexivity.
+Qed.
 (** [] *)
 
 (** **** Exercise: 2 stars (andb_eq_orb)  *)
