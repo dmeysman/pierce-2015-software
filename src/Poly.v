c3fa01b7bdabd93a2093aba815887fb5d84889da
Dylan Meysmans <dmeysman@vub.ac.be>
2015-10-01 01:38:48 +0200

    Implement prod_uncurry and prove uncurry_curry and curry_uncurry

--- src/Poly.v
+++ src/Poly.v
@@ -672,7 +672,9 @@ Definition prod_curry {X Y Z : Type}
 
 Definition prod_uncurry {X Y Z : Type}
   (f : X -> Y -> Z) (p : X * Y) : Z :=
-  (* FILL IN HERE *) admit.
+  match p with
+    | (x, y) => f x y
+  end.
 
 (** (Thought exercise: before running these commands, can you
     calculate the types of [prod_curry] and [prod_uncurry]?) *)
@@ -683,13 +685,17 @@ Check @prod_uncurry.
 Theorem uncurry_curry : forall (X Y Z : Type) (f : X -> Y -> Z) x y,
   prod_curry (prod_uncurry f) x y = f x y.
 Proof.
-  (* FILL IN HERE *) Admitted.
-
+  reflexivity.  
+Qed.
+  
 Theorem curry_uncurry : forall (X Y Z : Type)
                                (f : (X * Y) -> Z) (p : X * Y),
   prod_uncurry (prod_curry f) p = f p.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros X Y Z f p.
+  destruct p as [x y].
+  reflexivity.
+Qed.
 (** [] *)
 
 (* ###################################################### *)
