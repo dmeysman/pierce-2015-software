f5dbd782c358919b5359acf05754338446ea3a53
Dylan Meysmans <dmeysman@vub.ac.be>
2015-10-01 02:50:09 +0200

    Prove override_neq

--- src/Poly.v
+++ src/Poly.v
@@ -1108,7 +1108,12 @@ Theorem override_neq : forall (X:Type) x1 x2 k1 k2 (f : nat->X),
   beq_nat k2 k1 = false ->
   (override f k2 x2) k1 = x1.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros X x1 x2 k1 k2 f H I.
+  unfold override.
+  rewrite -> I.
+  rewrite -> H.
+  reflexivity.
+Qed.
 (** [] *)
 
 (** As the inverse of [unfold], Coq also provides a tactic
